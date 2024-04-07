df465a22cffbc869208bed8330d6c6897a4b317f
Alexcy <bondaralexcy@gmail.com>
2024-04-07 14:35:40 +0300

    Homework-4 Commit and HW-3 Updates

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -18,6 +18,16 @@
 	(select shipper_id from public.shippers where company_name = 'United Package') as s
 	on orders.ship_via = s.shipper_id;
 
+	-- Но лучше так:
+
+	SELECT customers.company_name as customer, CONCAT(employees.first_name, ' ', employees.last_name) AS emoloyee FROM customers
+    JOIN orders USING (customer_id)
+    JOIN employees USING (employee_id)
+    JOIN shippers ON orders.ship_via = shippers.shipper_id
+    WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Package';
+
+
+
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
@@ -41,6 +51,16 @@ from (
 )
 order by p.units_in_stock;
 
+-- Но лучше так:
+
+SELECT product_name, units_in_stock, contact_name, phone FROM products
+INNER JOIN suppliers USING (supplier_id)
+INNER JOIN categories USING (category_id)
+WHERE discontinued = 0 AND units_in_stock < 25 and category_name in('Dairy Products', 'Condiments')
+ORDER BY units_in_stock
+
+
+
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
 select company_name
@@ -54,6 +74,10 @@ from (
 
 	where o.orders_count is null
 	);
+
+-- Но лучше так:
+SELECT company_name FROM customers
+WHERE customer_id NOT IN (SELECT customer_id FROM orders)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
